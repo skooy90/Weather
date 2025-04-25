6101ff33ff28ad582fbfe00a3398a4c37e6df5ab
unknown <skooy90@gmail.com>
2025-04-25 18:09:52 +0900

    fix: 프론트엔드 CommonJS 문법을 ES 모듈 문법으로 수정 및 기타 변경사항

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
-const React = require('react');
-const ReactDOM = require('react-dom/client');
-const { BrowserRouter } = require('react-router-dom');
-const App = require('./App');
-require('./index.css');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
